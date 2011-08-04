eb235748ff98dc6a07440170fdaf90ca4f1cfd0c
tux-oid <tuxoiduser@gmail.com>
2011-08-04 17:48:01 +0400

    added words if filthylang filter

--- filters/filthylang/filter.php
+++ filters/filthylang/filter.php
@@ -3,7 +3,9 @@ class filthylang
 {
 	function check($message)
 	{
-		$cyrilic = array('А', 'В', 'Е');
+		$src = array('A', '6', 'B', '8', '9', 'E', 'Ё', '3', 'Й', 'K', 'M', 'H', 'O', '0', 'P', 'C', 'T', 'Y', 'X', '4', 'a', '6', 'b', '8', '9', 'e', 'ё', '3', 'й', 'k', 'm', 'h', 'o', '0', 'p', 'c', 't', 'y', 'x', '4');
+		$trgt = array('А', 'Б', 'В', 'В', 'Д', 'Е', 'Е', 'З', 'И', 'К', 'М', 'Н', 'О', 'О', 'Р', 'С', 'Т', 'У', 'Х', 'Ч', 'а', 'б', 'в', 'в', 'д', 'е', 'е', 'з', 'и', 'к', 'м', 'н', 'о', 'о', 'р', 'с', 'т', 'у', 'х', 'ч');
+		$message = str_replace($src, $trgt, $message);
 		$path = 'filters/filthylang/hui.txt';
 		if(file_exists($path))
 			$hui = file($path);
@@ -16,6 +18,30 @@ class filthylang
 		else
 			$her = array();
 		$her = core::trim_array($her);
+		$path = 'filters/filthylang/pizda.txt';
+		if(file_exists($path))
+			$pizda = file($path);
+		else
+			$pizda = array();
+		$pizda = core::trim_array($pizda);
+		$path = 'filters/filthylang/suka.txt';
+		if(file_exists($path))
+			$suka = file($path);
+		else
+			$suka = array();
+		$suka = core::trim_array($suka);
+		$path = 'filters/filthylang/bljad.txt';
+		if(file_exists($path))
+			$bljad = file($path);
+		else
+			$bljad = array();
+		$bljad = core::trim_array($bljad);
+		$path = 'filters/filthylang/eblya.txt';
+		if(file_exists($path))
+			$eblya = file($path);
+		else
+			$eblya = array();
+		$eblya = core::trim_array($eblya);
 		$message = strtolower($message);
 		$msg_words = explode(" ", $message);
 		for($i=0; $i<count($msg_words); $i++)
@@ -24,6 +50,14 @@ class filthylang
 			if(in_array($word , $hui))
 				return 1;
 			else if(in_array($word , $her))
+				return 1;
+			else if(in_array($word , $pizda))
+				return 1;
+			else if(in_array($word , $suka))
+				return 1;
+			else if(in_array($word , $bljad))
+				return 1;
+			else if(in_array($word , $eblya))
 				return 1;
 		}
 		return 0;
