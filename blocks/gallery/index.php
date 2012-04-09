2300056e69ba9454f85571354bec45fc9efcab52
Vladimir Gorbunov <truedaemon@gmail.com>
2012-04-10 01:54:27 +0400

    Rewrite gallery block

--- blocks/gallery/index.php
+++ blocks/gallery/index.php
@@ -1,37 +1,25 @@
 <?php
 $filename = 'blocks/'.$directory.'/templates/top.tpl.php';
-$file = fopen($filename, "r") or die("Can't open file!");
+$file = fopen($filename, "r") or die("Can't open gallery block top template");
 $boxlet_content = fread($file, filesize($filename));
 fclose($file); 
-$cnt = base::query('SELECT count(*) AS cnt FROM threads WHERE section=3', 'assoc_array', array());
-if($cnt[0]['cnt']>2)
-$max=3;
-else
+
+$ret = base::query('SELECT t.id, t.cid, c.subject, t.file, t.extension, u.nick, c.timest FROM threads t INNER JOIN comments c ON t.cid=c.id INNER JOIN users u ON c.uid=u.id WHERE t.approved=true AND t.section=3 ORDER BY t.id DESC LIMIT 3', 'assoc_array', array());
+
+$item_tpl = 'blocks/'.$directory.'/templates/middle.tpl.php';
+$tpl_file = fopen($item_tpl, "r") or die("Can't open gallery block middle template");
+$tpl_size = filesize($item_tpl);
+foreach ($ret as $item)
 {
-	if($cnt[0]['cnt']>0)
-		$max=$cnt[0]['cnt'];
-	else
-		$max = 0;
+	rewind($tpl_file);
+	$boxlet_content .= fread($tpl_file, $tpl_size);
+	$boxlet_content = str_replace('[author]', $item['nick'], $boxlet_content);
+	$boxlet_content = str_replace('[img_thumb_link]', 'images/gallery/thumbs/'.$item['file'].'_small.png', $boxlet_content);
+	$boxlet_content = str_replace('[img_link]', 'images/gallery/'.$item['file'].'.'.$item['extension'], $boxlet_content);
+	$boxlet_content = str_replace('[subject]', $item['subject'], $boxlet_content);
+	$boxlet_content = str_replace('[timestamp]', core::to_local_time_zone($item['timest']), $boxlet_content);
+	$boxlet_content = str_replace('[link]', 'thread_'.$item['id'].'_page_1', $boxlet_content);
 }
-for($y=0; $y<$max; $y++)
-{
-	$filename = 'blocks/'.$directory.'/templates/middle.tpl.php';
-	$file = fopen($filename, "r") or die("Can't open file!");
-	$boxlet_content = $boxlet_content.fread($file, filesize($filename));
-	fclose($file); 
-	$sel = base::query('SELECT max(id) AS max FROM threads WHERE section = 3','assoc_array');
-	if(empty($end))
-		$end = $sel[0]['max'];
-	$ret = base::query('SELECT t.id, t.cid, c.subject, t.file, t.file_size, t.image_size, t.extension, c.uid, c.timest FROM threads t INNER JOIN comments c ON t.cid = c.id WHERE t.approved=true AND t.section=3 ORDER BY id DESC LIMIT 3', 'assoc_array', array());
-	$where_arr = array(array("key"=>'id', "value"=>$ret[$y]['uid'], "oper"=>'='));
-	$author = base::select('users', '', 'nick', $where_arr);
-	$boxlet_content = str_replace('[author]', $author[0]['nick'], $boxlet_content);
-	$boxlet_content = str_replace('[img_thumb_link]', 'images/gallery/thumbs/'.$ret[$y]['file'].'_small.png', $boxlet_content);
-	$boxlet_content = str_replace('[img_link]', 'images/gallery/'.$ret[$y]['file'].'.'.$ret[$y]['extension'], $boxlet_content);
-	$boxlet_content = str_replace('[subject]', $ret[$y]['subject'], $boxlet_content);
-	$timest = core::to_local_time_zone($ret[$y]['timest']);
-	$boxlet_content = str_replace('[timestamp]', $timest, $boxlet_content);
-	$boxlet_content = str_replace('[link]', 'thread_'.$ret[$y]['id'].'_page_1', $boxlet_content);
-}
+fclose($tpl_file);
 $boxlet_content = str_replace('[title]', $name, $boxlet_content);
 ?>
