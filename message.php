d004a77725516e43943cf4945f30575f3b3533f3
Vladimir Gorbunov <truedaemon@gmail.com>
2012-04-15 20:57:01 +0400

    Fix links titles on message page

--- message.php
+++ message.php
@@ -72,7 +72,7 @@ if($pages_count > 1)
 	if($page>1)
 	{
 		$pg = $page-1;
-		$pages = $pages.'<a href="thread_'.$thread_id.'_page_1" title=В Начало>←</a>&nbsp;';
+		$pages = $pages.'<a href="thread_'.$thread_id.'_page_1" title="В начало">←</a>&nbsp;';
 		$pages = $pages.'<a href="thread_'.$thread_id.'_page_'.$pg.'" title="Назад">≪</a>&nbsp;';
 	}
 	if($pages_count>10)
@@ -108,7 +108,7 @@ if($pages_count > 1)
 	{
 		$pg = $page+1;
 		$pages = $pages.'<a href="thread_'.$thread_id.'_page_'.$pg.'" title="Вперед">≫</a>&nbsp;';
-		$pages = $pages.'<a href="thread_'.$thread_id.'_page_'.$pages_count.'" title="В Конец">→</a>&nbsp;';
+		$pages = $pages.'<a href="thread_'.$thread_id.'_page_'.$pages_count.'" title="В конец">→</a>&nbsp;';
 	}
 }
 require 'themes/'.$theme.'/templates/message/nav_form.tpl.php';
